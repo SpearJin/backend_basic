e3e2e9777bba3983b28b79f3e2aff9ac19f34de1
SpearJin <ckdwls2525@gmail.com>
2022-02-09 17:09:03 +0900

    회원가입, 로그인, 암호화 구현

--- app.js
+++ app.js
@@ -1,49 +1,47 @@
 const { json } = require('express');
+const argon2 = require('argon2');
 const express = require('express');
 const app = express();
 
-const database = [
-  { id: 1, title: '글1' },
-  { id: 2, title: '글2' },
-  { id: 3, title: '글3' },
-];
+const database = [{ id: 1, username: 'abc', password: 'abcdd' }];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// 정보 불러오기
-app.get('/database', function (req, res) {
+// 유저 정보 가져오기
+app.get('/users', function (req, res) {
   res.send(database);
 });
 
-// 특정 정보만 불러오기
-app.get('/database/:id', function (req, res) {
-  const id = req.params.id;
-  const data = database.find((item) => item.id === Number(id));
-  res.send(data);
+// 회원가입
+app.post('/signup', async (req, res) => {
+  // abc -> fwelfkjwlef
+  const { username, password, age, birthday } = req.body;
+  // argon2라이브러리를 통해 password 암호화 시킨다
+  const hash = await argon2.hash(password);
+  database.push({
+    username,
+    password: hash,
+    age,
+    birthday,
+  });
+  res.send('success');
 });
 
-// 정보 추가
-app.post('/database', function (req, res) {
-  const id = database.length + 1;
-  const title = req.body.title;
-  database.push({ id, title });
-  res.send('추가!!!');
-});
-
-// 정보 수정
-app.put('/database', function (req, res) {
-  const id = req.body.id;
-  const title = req.body.title;
-  database[id - 1].title = title;
-  res.send('수정!!!');
-});
-
-// 정보 삭제
-app.delete('/database', function (req, res) {
-  const id = req.body.id;
-  database.splice(id - 1, 1);
-  res.send('삭제!!!');
+// 로그인
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = database.filter((user) => user.username === username);
+  if (user.length === 0) {
+    res.status(403).send('해당 하는 id가 없습니다');
+    return;
+  }
+  // password는 암호화가 되었기 때문에 해독을 하고 비교를 한다
+  if (!(await argon2.verify(user[0].password, password))) {
+    res.status(403).send('패스워드가 틀립니다'); // 앞이 4이면 클라이언트 잘못, 5는 서버 문제
+    return;
+  }
+  res.send('로그인 성공');
 });
 
 app.listen(3000, () => {
